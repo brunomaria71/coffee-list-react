643744dad7b84e008fe3f4f0aea3b396d89958c9
brunomaria71 <brunomaria71@yahoo.com>
2022-05-17 15:12:13 -0400

    fetched an api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import CCoffee from './components/CCoffee';
+// import CCoffee from './components/CCoffee';
 import FCoffee from './components/FCoffee';
 
 function App() {
   return (
     <>
-    <CCoffee firstName="Maria"/>  
+    {/* <CCoffee firstName="Maria"/>   */}
     <FCoffee firstName="Maria"/>
     </>
   );
--- src/components/FCoffee.js
+++ src/components/FCoffee.js
@@ -1,18 +1,38 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 
 
 function FCoffee({ firstName }) {
     const [count, setCount] = useState(0);
+    const [coffeeList, setCoffeeList] = useState();
+    const [temperature, setTemperature] = useState("iced");
+    useEffect(()=>{
+        fetch (`https://api.sampleapis.com/coffee/${temperature}`)
+        .then(response => response.json())
+        .then(data => setCoffeeList(data)) // or .then(setCoffeeList)
+        .catch(console.error)
+    }, [temperature])
     const handleClick = () => {
         setCount(count +1);
     }
+    // console.log({coffeeList}) we generally get rid of the console.logs to clean it up. its really just for us to test it out.
     return (
         <>
             <h1>Coffee List (F)</h1>
             <p> Hello {firstName}</p>
             <p>You clicked the button {count} times.</p>
         <button onClick={handleClick}>Click me!</button>
+        {!coffeeList
+            ? <h2>Loading...</h2>
+            : <>
+            <h2>Coffees</h2>
+            {coffeeList.map(coffee => {
+                return <p key={coffee.id}>{coffee.title}</p>
+
+            })}
+            </>
+            
+        }
 
         </>
     )
