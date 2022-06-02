bac5c83433d9b6b4a3f7794be79767d075660895
Damian Montero <thedamian@users.noreply.github.com>
2022-06-02 02:14:36 -0400

    Suggested changes to make the terms report correctly and have the "submit" button work.

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,37 +1,33 @@
 import { useEffect, useState } from "react";
 
 export default function ContactForm() {
-  const [formData, setFormData] = useState({})
-  const [terms, setTerms] = useState(false) // initially it will be false then as it changes we can set the terms
-  const [validForm, setValidForm] = useState(false)
+  const [formData, setFormData] = useState({});
+  const [validForm, setValidForm] = useState(false);
 
   useEffect(() => {
-    if(formData.firstName && formData.lastName && formData.terms){
-    setValidForm(true)
-  }
-}, [formData.firstName, formData.lastName, formData.terms])
-
+    if (formData.firstName && formData.lastName && formData.terms) {
+      setValidForm(true);
+    }
+  }, [formData.firstName, formData.lastName, formData.terms]);
 
   const sendData = (e) => {
-    e.preventDefault() // right here we're saying do not refresh the page, just do the next line. 
+    e.preventDefault(); // right here we're saying do not refresh the page, just do the next line.
 
-    fetch (`${process.env.REACT_API_ENDPOINT}/hot`, 
-    {method: 'POST', headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(formData)
+    fetch(`${process.env.REACT_API_ENDPOINT}/hot`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formData)
     })
-    .then(res => res.json())
-    .then(data => console.log('Success!', data))
-    .catch(err => console.error(err))
-  }
+      .then((res) => res.json())
+      .then((data) => console.log("Success!", data))
+      .catch((err) => console.error(err));
+  };
 
-const setFormObject = (e) => {
-  setFormData({...formData, [e.target.name]: e.target.value})
-
-}
-
-console.log(formData)
+  const setFormObject = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   // everything inside of the return has to be html/jsx, we throw JS with curly braces
   return (
@@ -39,31 +35,28 @@ console.log(formData)
       <form method="post">
         <label>
           First Name:
-          <input 
-          type="text"  
-          name="firstName" 
-          placeholder="First Name" 
-          // onChange={(event) => setFormData({...formData, firstName: event.target.value})} />
-          onChange={(event) => setFormObject(event)} />
-
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+            // onChange={(event) => setFormData({...formData, firstName: event.target.value})} />
+            onChange={(event) => setFormObject(event)}
+          />
         </label>
         <br />
         <label>
           Last Name:
-          <input 
-          type="text" 
-          name="lastName" 
-          onChange={setFormData} />
+          <input type="text" name="lastName" onChange={setFormData} />
         </label>
         <br />
         <label>
           Address:
-          <input type="text" name="address" onChange={setFormData}/>
+          <input type="text" name="address" onChange={setFormData} />
         </label>
         <br />
         <label>
           Zip:
-          <input type="text" name="zip" maxLength={5} onChange={setFormData}/>
+          <input type="text" name="zip" maxLength={5} onChange={setFormData} />
         </label>
 
         <br />
@@ -78,26 +71,32 @@ console.log(formData)
         </label>
         <br />
 
-        <label>Date:
-        <input type="date" name="date" />
+        <label>
+          Date:
+          <input type="date" name="date" />
         </label>
 
         <br />
         <label>
-        Terms and conditions
-        <input type="checkbox" onChange={(event) => setTerms(event.target.checked)}/>
+          Terms and conditions
+          <input
+            type="checkbox"
+            name="terms"
+            onChange={(event) => setValidForm(event.target.checked)}
+          />
         </label>
         <br />
 
-        <label >message:
-          <textarea name="message"  cols="30" rows="10" />
+        <label>
+          message:
+          <textarea name="message" cols="30" rows="10" />
         </label>
         <br />
 
         <button onClick={(e) => sendData(e)} disabled={!validForm}>
-          Submit</button>
+          Submit
+        </button>
       </form>
     </>
-  )
-};
-
+  );
+}
